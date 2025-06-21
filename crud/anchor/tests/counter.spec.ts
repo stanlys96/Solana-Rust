a86815e518aae826e7606d750b76e6dc535f7633
stanlys96 <stanlyskwok@gmail.com>
2025-06-21 23:07:31 +0700

    wlao

--- crud/anchor/tests/counter.spec.ts
+++ crud/anchor/tests/counter.spec.ts
@@ -10,12 +10,66 @@ const journalAddress = new PublicKey(
 );
 
 describe('crudapp', () => {
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   let crudProgram = anchor.workspace.Crudapp as Program<Crudapp>;
 
-  it('Create a journal entry', async () => {
+  it('Read the journal entry', async () => {
+    const currentTitle = 'Yoel Romero';
+    const currentDescription = 'I am the champion!';
     await crudProgram.methods
-      .createJournalEntry('Hello world!!!', 'I am the champion!')
+      .createJournalEntry(currentTitle, currentDescription)
       .rpc();
+    const [journalEntryPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(currentTitle), provider.wallet.publicKey.toBuffer()],
+      crudProgram.programId
+    );
+    const journalAccount = await crudProgram.account.journalEntryState.fetch(
+      journalEntryPDA
+    );
+    console.log(journalEntryPDA);
+    console.log(journalAccount);
+    await crudProgram.methods.deleteJournalEntry(currentTitle).rpc();
+  });
+
+  it('Fetch all journal entries', async () => {
+    const allJournalEntries = await crudProgram.account.journalEntryState.all();
+
+    for (const entry of allJournalEntries) {
+      console.log(entry);
+      console.log('-----------');
+    }
+  });
+
+  it('Create a journal entry', async () => {
+    const currentTitle = 'Israel Adesanya';
+    const currentDescription = 'I am the champion!';
+    await crudProgram.methods
+      .createJournalEntry(currentTitle, currentDescription)
+      .rpc();
+    const [journalEntryPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(currentTitle), provider.wallet.publicKey.toBuffer()],
+      crudProgram.programId
+    );
+    const journalAccount = await crudProgram.account.journalEntryState.fetch(
+      journalEntryPDA
+    );
+    expect(journalAccount.title).toEqual(currentTitle);
+    expect(journalAccount.message).toEqual(currentDescription);
+    const updatedDescription = 'I am the double champion!';
+    await crudProgram.methods
+      .updateJournalEntry(currentTitle, updatedDescription)
+      .rpc();
+    const [secondJournalEntryPDA] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from(currentTitle), provider.wallet.publicKey.toBuffer()],
+        crudProgram.programId
+      );
+    const secondJournalAccount =
+      await crudProgram.account.journalEntryState.fetch(secondJournalEntryPDA);
+    expect(secondJournalAccount.message).toEqual(updatedDescription);
+    console.log(secondJournalEntryPDA);
+    console.log(secondJournalAccount);
+    await crudProgram.methods.deleteJournalEntry(currentTitle).rpc();
   });
 });
